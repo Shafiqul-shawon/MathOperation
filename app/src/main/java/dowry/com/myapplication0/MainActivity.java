c7a5b2265fbb25ca83ba38e524e0327cc1278910
ShafiqulShawon <shawon120115@gmail.com>
2018-05-30 00:55:50 +0600

    Faltu button ta delete korsi

--- app/src/main/java/dowry/com/myapplication0/MainActivity.java
+++ app/src/main/java/dowry/com/myapplication0/MainActivity.java
@@ -1,8 +1,7 @@
 package dowry.com.myapplication0;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -63,13 +62,4 @@ public class MainActivity extends AppCompatActivity {
         t1.setText(Integer.toString(div));
 
     }
-
-    public void openPractiseApp(View v){
-        Intent intent = new Intent("mist.com.practise.MainActivity");
-        startActivity(intent);
-    }
-
-// baal chaal
-// baal chaal 2
-
 }
